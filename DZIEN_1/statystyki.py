5722ef0cd0b15def2dbd102ea73957b430d4939c
albim72 <marcin.albiniak72@gmail.com>
2024-06-24 14:58:53 +0200

    Update statystyki.py

--- DZIEN_1/statystyki.py
+++ DZIEN_1/statystyki.py
@@ -1,1 +1,15 @@
 liczby = [877,34,56,-122,4,1,66,-234,135,689,1111,0,-988,1.22,89,13,56,1356,-345,-1000,16,19]
+
+def pokaz_statystyki(dane:list)->tuple:
+    minimum:float = min(dane)
+    maksimum:float = max(dane)
+    roztep:float = maksimum - minimum
+    sr_arytm:float = sum(dane)/len(dane)
+    return minimum,maksimum,roztep,sr_arytm
+
+wynik = pokaz_statystyki(liczby)
+print(wynik)
+print(type(wynik))
+
+mini,maxi,roz,srednia = pokaz_statystyki(liczby)
+print(f'wartość największa: {maxi}, wartość najmniejsza: {mini}, rozstęp: {roz}, średnia: {srednia}')
