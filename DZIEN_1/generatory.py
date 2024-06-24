136dc0400354197d66ef346be4ca6d551ea21bc8
albim72 <marcin.albiniak72@gmail.com>
2024-06-24 14:01:50 +0200

    Update generatory.py

--- DZIEN_1/generatory.py
+++ DZIEN_1/generatory.py
@@ -9,3 +9,33 @@ print(list(liczby()))
 
 for p in liczby():
     print(p)
+
+
+#przyklad 2
+
+def resumption(n,k):
+    print("wstrzymanie działania 0")
+    yield 1003
+    print("wznowienie działania 1")
+    yield n+k
+    print("wstrzymanie działania 1")
+    yield n-k
+    print("wznowienie działania 2")
+    print("wstrzymanie działania 2")
+    n = 8*k-2
+    k=3*k
+    yield n*k
+    print("wznowienie działania 3")
+    print("wstrzymanie działania 3")
+    yield n/k
+    print("wznowienie działania 4")
+    yield "to jest ostatni element generatora"
+
+print(resumption(7,8))
+print(tuple(resumption(7,8)))
+print("_"*50)
+for i in resumption(7,8):
+    print("*"*35)
+    print(type(i))
+    print(f'zwrócono wartośćL {i}')
+print("_"*50)
