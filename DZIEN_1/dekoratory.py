7bbf34ef0e92d7b142a710ebe9ca74448a375cef
albim72 <marcin.albiniak72@gmail.com>
2024-06-24 12:21:16 +0200

    Update dekoratory.py

--- DZIEN_1/dekoratory.py
+++ DZIEN_1/dekoratory.py
@@ -22,3 +22,37 @@ def big_list():
     sum([i**5 for i in range(10_000_000)])
 
 big_list()
+
+
+print("_________ decorator debug __________")
+
+def debug(func):
+    def wrapper(*args):
+        print(f'call function: {func.__name__}')
+        func(*args)
+    return wrapper
+# def wrapper(func,*args):
+#     print(f'call function: {func.__name__}')
+#     func(*args)
+
+@debug
+def info(i):
+    print(f'code: {i}')
+
+info("hbjefsjfdkl303480423049")
+
+print("_________ dekorator z parametrem __________")
+
+def repeat(n):
+    def wrapper(func):
+        def inner(*args):
+            for i in range(n):
+                func(*args)
+        return inner
+    return wrapper
+
+@repeat(7)
+def calc(c,d):
+    print(f'result -> {c*d**2}')
+
+calc(2,3)
