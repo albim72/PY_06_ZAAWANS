b81908def22160a29be214c0f0adbb814f44171b
albim72 <marcin.albiniak72@gmail.com>
2024-06-25 15:07:25 +0200

    Update main.py

--- DZIEN_2/FIGURY_GEO/main.py
+++ DZIEN_2/FIGURY_GEO/main.py
@@ -1,4 +1,11 @@
 from triangle import Triangle
+from rectangle import Rectangle
 
 tr = Triangle(5.6,7.2)
 print(f'pole figury {tr.__class__.__name__} wynosi: {tr.calc_area()} cm2')
+
+rc1 = Rectangle(4,5.8)
+print(f'pole figury {rc1.__class__.__name__} wynosi: {rc1.calc_area()} cm2')
+
+rc2 = Rectangle(5.5,5.5)
+print(f'pole figury {rc2.__class__.__name__} wynosi: {rc2.calc_area()} cm2')
