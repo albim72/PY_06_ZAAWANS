7008a6f8c756c6bb065267709e7ffd8d586165c2
albim72 <marcin.albiniak72@gmail.com>
2024-06-25 09:25:37 +0200

    Update main.py

--- DZIEN_1/PROJEKT_OSOBA/main.py
+++ DZIEN_1/PROJEKT_OSOBA/main.py
@@ -5,3 +5,13 @@ os1 = Osoba("Jan",46,102,173)
 print(os1)
 nlat = 8
 print(f'wiek za {nlat} lat: {os1.wiek_za_n_lat(nlat)}')
+print(f'Czy osoba jest pracownikiem? -> {os1.czypracownik()}')
+
+print("_"*50)
+os2 = Osoba("Anna",33,54,170)
+os2.set_kolor_oczu("niebieskie")
+print(os2)
+print(f"zmieniony kolor oczu -> {os2.get_kolor_oczu()}")
+nlat = 5
+print(f'wiek za {nlat} lat: {os2.wiek_za_n_lat(nlat)}')
+print(f'Czy osoba jest pracownikiem? -> {os2.czypracownik()}')
