1dc6f41a900681ff887a9bbcd9a268295e20fd8a
albim72 <marcin.albiniak72@gmail.com>
2024-06-25 13:44:59 +0200

    Update main.py

--- DZIEN_2/EGZAMINY/main.py
+++ DZIEN_2/EGZAMINY/main.py
@@ -1,7 +1,18 @@
 from homework import Homework
+from exam import Exam
 
 print("_________ HOMEWORK _________")
 g = Homework()
 g.grade = 95
 assert not(g.grade >= 5 and g.grade <=15)
 print(f'ocena za projekt: {g.grade}')
+
+print("_________ EXAM _________")
+ex = Exam()
+ex.part_a_grade = 72
+ex.part_b_grade = 79
+
+assert ex.part_a_grade >= 70
+assert ex.part_b_grade >= 50
+
+print(f'egzamin -> a -> {ex.part_a_grade}, b -> {ex.part_b_grade}')
