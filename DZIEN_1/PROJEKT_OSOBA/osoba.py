9151bfd9a4b0026d9a1743adb96728b8342bf962
albim72 <marcin.albiniak72@gmail.com>
2024-06-25 09:16:24 +0200

    Update osoba.py

--- DZIEN_1/PROJEKT_OSOBA/osoba.py
+++ DZIEN_1/PROJEKT_OSOBA/osoba.py
@@ -7,16 +7,21 @@ class Osoba:
         self.wiek = wiek
         self.waga = waga
         self.wzrost = wzrost
+        self.kolor_oczu = "brązowe"
         self.info()
 
     def __repr__(self):
         return (f'reprezentacja tekstowa obiektu -> {self.imie}\n'
                 f'wiek /lata/: {self.wiek}\n'
                 f'wzrost /cm/: {self.wzrost}\n'
-                f'waga /kg/: {self.waga}\n')
+                f'waga /kg/: {self.waga}\n'
+                f'kolor oczu - {self.kolor_oczu}')
 
     def info(self):
         print(f"Utworzono obiekt klasy: {self.__class__.__name__}")
 
     def wiek_za_n_lat(self,n):
         return self.wiek + n
+
+    def czypracownik(self)->bool:
+        return False
