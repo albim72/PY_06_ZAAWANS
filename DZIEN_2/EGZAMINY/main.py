8fe111bdade9c1c086eff9e2c0b904ca6ac48dbb
albim72 <marcin.albiniak72@gmail.com>
2024-06-25 14:01:29 +0200

    Update main.py

--- DZIEN_2/EGZAMINY/main.py
+++ DZIEN_2/EGZAMINY/main.py
@@ -1,5 +1,6 @@
 from homework import Homework
 from exam import Exam
+from grade import ExamD
 
 print("_________ HOMEWORK _________")
 g = Homework()
@@ -16,3 +17,25 @@ assert ex.part_a_grade >= 70
 assert ex.part_b_grade >= 50
 
 print(f'egzamin -> a -> {ex.part_a_grade}, b -> {ex.part_b_grade}')
+
+print("_________ EXAM D_________")
+first = ExamD()
+first.math_grade = 34
+first.alg_grade = 45
+first.prog_grade = 55
+
+print(f'pierwsze podejście -> \nmatematyka -> {first.math_grade}\nalgorytmika -> {first.alg_grade}\n'
+      f'programowanie {first.prog_grade}')
+
+second = ExamD()
+second.math_grade = 64
+second.alg_grade = 73
+second.prog_grade = 88
+
+print("_" * 50)
+print(f'drugie podejście -> \nmatematyka -> {second.math_grade}\nalgorytmika -> {second.alg_grade}\n'
+      f'programowanie {second.prog_grade}')
+
+print("_" * 50)
+print(f'Archiwum --> pierwsze podejście -> \nmatematyka -> {first.math_grade}\nalgorytmika -> {first.alg_grade}\n'
+      f'programowanie {first.prog_grade}')
