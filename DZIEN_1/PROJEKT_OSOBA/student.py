8f1689f0afb9b71952860baa4c5fd155ee1b8bdb
albim72 <marcin.albiniak72@gmail.com>
2024-06-25 10:32:49 +0200

    Create student.py

---
+++ DZIEN_1/PROJEKT_OSOBA/student.py
@@ -0,0 +1,42 @@
+from pracownik import Pracownik
+from osoba import Osoba
+
+class Student(Pracownik):
+    def __init__(self, imie, wiek, waga, wzrost, wydzial, kierunek, rok_stud, firma=None, stanowisko=None,
+                 lata_pracy=None, wynagrodzenie=None, dyscyplina=None,lataupr=None, bestwynik=None):
+        super().__init__(imie, wiek, waga, wzrost, firma, stanowisko, lata_pracy, wynagrodzenie, dyscyplina, lataupr,
+                         bestwynik)
+        self.wydzial = wydzial
+        self.kierunek = kierunek
+        self.rok_stud = rok_stud
+
+    def __repr__(self):
+        nrep = Osoba.__repr__()
+
+        dane_f = (f"firma: {self.firma}\n"
+                  f"stanowisko pracy: {self.stanowisko}\n"
+                  f"lata pracy: {self.lata_pracy} \n"
+                  f"wynagrodzenie: {self.wynagrodzenie:.2f} zł\n")
+        dane_s = (f"dyscyplina: {self.dyscyplina}\n"
+                    f"lata uprawiania: {self.lataupr}\n"
+                    f"życiówka: {self.bestwynik}\n")
+
+        stud = (f"wydział: {self.wydzial}\n"
+                f"kierunek: {self.kierunek}\n"
+                f"rok studiów: {self.rok_stud}\n")
+
+
+        if self.firma == None & self.dyscyplina == None:
+            return nrep + stud
+        elif self.firma == None:
+            return nrep + stud + dane_s
+        elif self.dyscyplina == None:
+            return nrep + stud + dane_f
+        else:
+            return nrep + stud + dane_f + dane_s
+
+    def czypracownik(self) -> bool:
+        return self.firma != None
+        
+    
+
