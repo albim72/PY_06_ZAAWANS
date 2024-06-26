8bf32f060cdc105bb7f59f5b56c47557396266db
albim72 <marcin.albiniak72@gmail.com>
2024-06-26 14:53:08 +0200

    Update filozof.py

--- DZIEN_3/filozof.py
+++ DZIEN_3/filozof.py
@@ -7,13 +7,19 @@ else:
 def odpowiedz(self):
     return "Tak! Ziemia jest płaska!"
 
+def nowa_odpowiedz(self):
+    return "Nie! Ziemia jest elipsoidą!"
+
 def brak(self):
     return "brak odpowiedzi..."
 
 class SednoOdpowiedzi(type):
     def __init__(cls,clsname,bases,attrs):
         if required:
-            cls.odpowiedz = odpowiedz
+            if attrs.get('n'):
+                cls.odpowiedz = nowa_odpowiedz
+            else:
+                cls.odpowiedz = odpowiedz
         else:
             cls.odpowiedz = brak
 
@@ -28,13 +34,18 @@ class SwTomasz(metaclass=SednoOdpowiedzi):
     pass
 
 class Kopernik(metaclass=SednoOdpowiedzi):
-    pass
+    n = True
+
+class Einstein(metaclass=SednoOdpowiedzi):
+    n = True
 
 fil1 = Arystoteles()
 fil2 = Platon()
 fil3 = SwTomasz()
 fil4 = Kopernik()
+fil5 = Einstein()
 print(f'filozof: {fil1.__class__.__name__} twierdzi: {fil1.odpowiedz()}')
 print(f'filozof: {fil2.__class__.__name__} twierdzi: {fil2.odpowiedz()}')
 print(f'filozof: {fil3.__class__.__name__} twierdzi: {fil3.odpowiedz()}')
 print(f'filozof: {fil4.__class__.__name__} twierdzi: {fil4.odpowiedz()}')
+print(f'filozof: {fil5.__class__.__name__} twierdzi: {fil5.odpowiedz()}')
