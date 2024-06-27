7222da379e1347808126b6c4d630da4e1fd66398
albim72 <marcin.albiniak72@gmail.com>
2024-06-27 11:14:51 +0200

    Update bookProtocol.py

--- DZIEN_4/bookProtocol.py
+++ DZIEN_4/bookProtocol.py
@@ -4,7 +4,7 @@ from typing import Protocol
 class Printable(Protocol):
     def print_info(self)->None:
         pass
-    
+
 #klasa inmplementująca protokół
 class Book(Printable):
     def __init__(self,title:str,author:str)->None:
@@ -22,16 +22,26 @@ class Magazine(Printable):
 
     def print_info(self) -> None:
         print(f"Magazyn {self.name}, wydanie: {self.issue}")
-        
-        
+
+
 class Test:
     def __init__(self,name:str,info:str)->None:
         self.name = name
         self.info = info
-        
+
     def print_info(self) -> None:
-        print(f"Testowy {self.name}, informacje: {self.issue}")
-    
-        
-    
-    
+        print(f"Testowy {self.name}, informacje: {self.info}")
+
+#funkcja, która przyjmuje obiekty zgodne z protokołem Printable
+def print_details(item:Printable)->None:
+    item.print_info()
+
+
+book = Book("Hobbit","J.R.R. Tolkien")
+magazine = Magazine("Skarby historii",'44')
+print_details(book)
+print_details(magazine)
+
+print("wywołanie obiektu niezgodnego z protokołem Printable...")
+tst = Test("materiały xyz","wariant 4")
+print_details(tst)
